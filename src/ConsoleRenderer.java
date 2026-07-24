42c9e0ca454b4791bde5a811b7d5ef04a152ce3e
khksamuel <a1821027@student.adelaide.edu.au>
2026-07-24 19:54:19 +0930

    clean unused colour var

--- src/ConsoleRenderer.java
+++ src/ConsoleRenderer.java
@@ -1,12 +1,4 @@
 public class ConsoleRenderer implements Renderer {
-    private static final String ANSI_RESET = "\u001B[0m";
-    private static final String ANSI_RED = "\u001B[31m";
-    private static final String ANSI_GREEN = "\u001B[32m";
-    private static final String ANSI_YELLOW = "\u001B[33m";
-    private static final String ANSI_WHITE = "\u001B[37m";
-    // 256-color orange. Falls back to terminal default if unsupported.
-    private static final String ANSI_ORANGE = "\u001B[38;5;208m";
-
     @Override
     public void print(String message) {
         System.out.println(message);
